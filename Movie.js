0c4a25304ffb3d6aed06746c83a9f8cd14d29dc3
vincentloy33 <67559831+vincentloy33@users.noreply.github.com>
2020-07-18 08:33:47 +0800

    Add files via upload

---
+++ Movie.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://Vincent:Vincent@123@cluster0.plzn3.mongodb.net/Movie?retryWrites=true&w=majority';
+
+
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch(error => {
+    console.log('Mongoose connetion error: ', error);
+  });
+
+const schema = mongoose.Schema({
+  title: { type: String },
+  year: { type: String },
+  genre: { type: String },
+  actors: { type: String },
+  plot: { type: String },
+  poster: { type: String }
+});
+
+const Movie = mongoose.model('Movie', schema, 'movieCollection');
+
+module.exports = Movie;
